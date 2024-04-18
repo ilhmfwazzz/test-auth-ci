5b3b3cc005e4c6e5686052278c1ca9b67aa1e413
ilhmfwazzz <ilhmfwaz02@gmail.com>
2024-04-18 12:38:54 +0700

    init

---
+++ app/Config/Routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+$routes->get('login', [\App\Controllers\Auth\LoginController::class, 'index']);
